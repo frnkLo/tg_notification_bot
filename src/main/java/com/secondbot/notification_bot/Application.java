99e1b51ccc1d915154fb7f02c43e03899b382611
frnkLo <i.nmchk@ya.ru>
2026-03-09 15:51:13 +0300

    deleted all comments

--- src/main/java/com/secondbot/notification_bot/Application.java
+++ src/main/java/com/secondbot/notification_bot/Application.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(TelegramProperties.class) //для работы тележки
+@EnableConfigurationProperties(TelegramProperties.class)
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
